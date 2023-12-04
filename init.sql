f1034ed226178b76555d334c437a83970e663e1e
Alexander <46569007+DreamsAreReal@users.noreply.github.com>
2023-12-04 04:34:48 +0300

    sql

---
+++ init.sql
@@ -0,0 +1,86 @@
+﻿DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '__EFMigrationsHistory') THEN
+CREATE TABLE "__EFMigrationsHistory" (
+                                         "MigrationId" character varying(150) NOT NULL,
+                                         "ProductVersion" character varying(32) NOT NULL,
+                                         CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'Houses') THEN
+CREATE TABLE "Houses" (
+                          "Id" uuid NOT NULL,
+                          "Number" integer NOT NULL,
+                          "CreatedAt" timestamp with time zone NOT NULL,
+                          "IsReadonly" boolean NOT NULL,
+                          "ModifiedAt" timestamp with time zone NOT NULL,
+                          CONSTRAINT "PK_Houses" PRIMARY KEY ("Id")
+);
+END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'Flats') THEN
+CREATE TABLE "Flats" (
+                         "Id" uuid NOT NULL,
+                         "Number" integer NOT NULL,
+                         "HouseId" uuid,
+                         "CreatedAt" timestamp with time zone NOT NULL,
+                         "IsReadonly" boolean NOT NULL,
+                         "ModifiedAt" timestamp with time zone NOT NULL,
+                         CONSTRAINT "PK_Flats" PRIMARY KEY ("Id"),
+                         CONSTRAINT "FK_Flats_Houses_HouseId" FOREIGN KEY ("HouseId") REFERENCES "Houses" ("Id") ON DELETE RESTRICT
+);
+END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'Bills') THEN
+CREATE TABLE "Bills" (
+                         "Id" uuid NOT NULL,
+                         "AmountInRubles" numeric NOT NULL,
+                         "FlatId" uuid NOT NULL,
+                         "PaidIn" timestamp with time zone,
+                         "CreatedAt" timestamp with time zone NOT NULL,
+                         "IsReadonly" boolean NOT NULL,
+                         "ModifiedAt" timestamp with time zone NOT NULL,
+                         CONSTRAINT "PK_Bills" PRIMARY KEY ("Id"),
+                         CONSTRAINT "FK_Bills_Flats_FlatId" FOREIGN KEY ("FlatId") REFERENCES "Flats" ("Id") ON DELETE RESTRICT
+);
+END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'Members') THEN
+CREATE TABLE "Members" (
+                           "Id" uuid NOT NULL,
+                           "Name" text NOT NULL,
+                           "FlatId" uuid,
+                           "CreatedAt" timestamp with time zone NOT NULL,
+                           "IsReadonly" boolean NOT NULL,
+                           "ModifiedAt" timestamp with time zone NOT NULL,
+                           CONSTRAINT "PK_Members" PRIMARY KEY ("Id"),
+                           CONSTRAINT "FK_Members_Flats_FlatId" FOREIGN KEY ("FlatId") REFERENCES "Flats" ("Id") ON DELETE RESTRICT
+);
+END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'ResidencyEventLogs') THEN
+CREATE TABLE "ResidencyEventLogs" (
+                                      "Id" uuid NOT NULL,
+                                      "EventType" integer NOT NULL,
+                                      "FlatId" uuid NOT NULL,
+                                      "MemberId" uuid NOT NULL,
+                                      "CreatedAt" timestamp with time zone NOT NULL,
+                                      "IsReadonly" boolean NOT NULL,
+                                      "ModifiedAt" timestamp with time zone NOT NULL,
+                                      CONSTRAINT "PK_ResidencyEventLogs" PRIMARY KEY ("Id"),
+                                      CONSTRAINT "FK_ResidencyEventLogs_Flats_FlatId" FOREIGN KEY ("FlatId") REFERENCES "Flats" ("Id") ON DELETE RESTRICT,
+                                      CONSTRAINT "FK_ResidencyEventLogs_Members_MemberId" FOREIGN KEY ("MemberId") REFERENCES "Members" ("Id") ON DELETE RESTRICT
+);
+END IF;
+
+END $$;
+
+
+START TRANSACTION;
+
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20231204013222_Initial', '8.0.0');
+
+COMMIT;
---
+++ src/ResidenceTracker.Infrastructure.DataAccess.PostgreSql/Migrations/20231204013222_Initial.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResidenceTracker.Infrastructure.DataAccess.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReadonly = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    HouseId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReadonly = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flats_Houses_HouseId",
+                        column: x => x.HouseId,
+                        principalTable: "Houses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AmountInRubles = table.Column<decimal>(type: "numeric", nullable: false),
+                    FlatId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PaidIn = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReadonly = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bills_Flats_FlatId",
+                        column: x => x.FlatId,
+                        principalTable: "Flats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    FlatId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReadonly = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Members_Flats_FlatId",
+                        column: x => x.FlatId,
+                        principalTable: "Flats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResidencyEventLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventType = table.Column<int>(type: "integer", nullable: false),
+                    FlatId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReadonly = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResidencyEventLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResidencyEventLogs_Flats_FlatId",
+                        column: x => x.FlatId,
+                        principalTable: "Flats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ResidencyEventLogs_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_FlatId",
+                table: "Bills",
+                column: "FlatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flats_HouseId",
+                table: "Flats",
+                column: "HouseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_FlatId",
+                table: "Members",
+                column: "FlatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResidencyEventLogs_FlatId",
+                table: "ResidencyEventLogs",
+                column: "FlatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResidencyEventLogs_MemberId",
+                table: "ResidencyEventLogs",
+                column: "MemberId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "ResidencyEventLogs");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Flats");
+
+            migrationBuilder.DropTable(
+                name: "Houses");
+        }
+    }
+}
